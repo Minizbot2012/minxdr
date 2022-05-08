2d3befbb67971c1f3049fd374866c2b508054e2f
Minizbot2012 <bradleyfwin@gmail.com>
2022-05-08 05:27:30 -0400

    v.string

--- decode.go
+++ decode.go
@@ -279,8 +279,7 @@ func (s *Decoder) decode(v reflect.Value) (int, error) {
 		return 0, err
 	}
 
-	println(v.Type().String())
-	if ecdc, ok := v.Interface().(EncodeDecode); ok {
+	if ecdc, ok := v.Elem().Interface().(EncodeDecode); ok {
 		return ecdc.Decode(s)
 	}
 
